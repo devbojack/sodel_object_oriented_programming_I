492e898eb2fe0a31cb98623deab0892a82da97fd
Justus Lolwerikoi <98508704+devbojack@users.noreply.github.com>
2025-06-25 21:52:18 +0300

    Updated question_2.cpp
    Updated to an object from a list of vehicles

--- question_2.cpp
+++ question_2.cpp
@@ -2,58 +2,45 @@
 #include <string>
 using namespace std;
 
-// Maximum number of allowed cars in the database
-const int MAX_CARS = 150;
-
-// Car structure that defines one car
-struct Car
-{
+// Vehicle class and its member functions
+class Vehicle {
+  private:
     std::string make;
     std::string model;
     std::string engineNumber;
-    double salePrice;
-};
-
-// Vehicle class with Car list and their member functions
-class Vehicle {
-  private:
-    Car cars[MAX_CARS];
-    int salesCount;
+    double salesPrice;
 
   public:
-    Vehicle(void)
+    Vehicle()
     {
-        salesCount = 0;
+        salesPrice = 0;
+    }
+    // Inserts the vehicle details
+    void set_vehicle(const std::string &carMake, const std::string &carModel,
+                     const std::string &carEngineNumber, double carSalesPrice)
+    {
+        make = carMake;
+        model = carModel;
+        engineNumber = carEngineNumber;
+        salesPrice = carSalesPrice;
     }
 
-    // Inserts a new car sales to the cars array
-    void set_vehicle(const std::string &make, const std::string &model,
-                     const std::string &engineNumber, double salesPrice)
+    // Returns the profit
+    double get_profit()
     {
-        if (salesCount < MAX_CARS)
-        {
-            cars[salesCount] = {make, model, engineNumber, salesPrice};
-            salesCount++;
-        }
-        else
-        {
-            std::cout << "Sales database is full!\n";
-        }
+        return salesPrice * 0.15;
     }
 
-    // Returns the total profit
-    void get_profit()
+    // Prints vehicle data and profit
+    void printVehicleData()
     {
-        if (salesCount > 0)
-        {
-            std::cout << "\n" << "Total Profit from the sale of " << salesCount << " cars. \n";
-            double totalProfit = 0;
-            for (int i = 0; i < salesCount; ++i)
-            {
-                totalProfit += cars[i].salePrice * 0.15;
-            }
-            std::cout << "Total Profit : " << totalProfit << "\n";
-        }
+        std::cout << "\n"
+                  << "Make:  " << make << "\n"
+                  << "Model:  " << model << "\n"
+                  << "Engine Number:  " << engineNumber << "\n"
+                  << "Price:  " << salesPrice << "\n"
+                  << "Profit:  " << get_profit() << "\n"
+                  << "\n";
     }
 };
 
@@ -61,9 +48,11 @@ int main(void)
 {
     Vehicle cars;
 
-    cars.set_vehicle("Nissan", "Sunny", "AOPXLAL", 240);
-    cars.set_vehicle("Toyota", "Harrier", "A12A7AA", 420);
-    cars.set_vehicle("Lexus", "RX450D", "JLSPALL", 300);
-    cars.get_profit();
+    cars.set_vehicle("Nissan", "Sunny", "AOPXLAL", 24000000);
+    cars.printVehicleData();
+    cars.set_vehicle("Toyota", "Harrier", "A12A7AA", 4200000);
+    cars.printVehicleData();
+    cars.set_vehicle("Lexus", "RX450D", "JLSPALL", 30000000);
+    cars.printVehicleData();
     return 0;
 }
