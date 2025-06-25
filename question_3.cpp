89e0407821a77d631f1f2c355eddd40d6f1a2cd6
Justus Lolwerikoi <98508704+devbojack@users.noreply.github.com>
2025-06-25 22:16:33 +0300

    Created question_3.cpp

---
+++ question_3.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// const int MAX_VOTERS = 5000;
+// VoteDetails structure that defines one Voter
+// struct VoterDetails
+// {
+//     string nationalIdNumber;
+//     string firstName;
+//     string middleName;
+//     string surname;
+//     string pollingStation;
+//     string dateOfBirth;
+//     string gender;
+// };
+
+class Voter {
+  private:
+    string voterCardId;
+    string nationalIdNumber;
+    string firstName;
+    string middleName;
+    string surname;
+    string pollingStation;
+    string dateOfBirth;
+    string gender;
+
+  public:
+    // Add the voter's details
+    void set_details()
+    {
+        cout << "Voter Card ID: ";
+        getline(cin, voterCardId);
+
+        cout << "National ID Number: ";
+        getline(cin, nationalIdNumber);
+
+        cout << "First Name: ";
+        getline(cin, firstName);
+
+        cout << "Middle Name: ";
+        getline(cin, middleName);
+
+        cout << "Surname: ";
+        getline(cin, surname);
+
+        cout << "Polling Station: ";
+        getline(cin, pollingStation);
+
+        cout << "Date of Birth (dd-mm-yyyy): ";
+        getline(cin, dateOfBirth);
+
+        cout << "Gender (M or F): ";
+        getline(cin, gender);
+    }
+
+    // Print the voter's details
+    void print_details() const
+    {
+        cout << "Voter Card ID: " << voterCardId << '\n';
+        cout << "National ID: " << nationalIdNumber << '\n';
+        cout << "First Name: " << firstName << '\n';
+        cout << "Middle Name: " << middleName << '\n';
+        cout << "Surname: " << surname << '\n';
+        cout << "Polling Station: " << pollingStation << '\n';
+        cout << "Date of Birth: " << dateOfBirth << '\n';
+        cout << "Gender: " << gender << '\n';
+    }
+};
+
+int main()
+{
+    Voter voter1;
+    voter1.set_details();
+    voter1.print_details();
+    return 0;
+}
